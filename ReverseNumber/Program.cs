b63f3b64e00ae8def74749ebe9c7e6dc02d648a9
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-13 18:16:09 +0530

    Reverse Number Program added.

---
+++ ReverseNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿using ReverseNumber;
+
+namespace Reversestore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Please enter number");
+            //int input = Convert.ToInt32(Console.ReadLine());
+            //ReverseProgram.ReverseNumber(input);
+            //Console.ReadLine();
+
+            ReverseProgram.ReverseWord("Shivani");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ReverseNumber/ReverseNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumber
+{
+    internal class ReverseProgram
+    {
+        //public static void ReverseNumber(int number)
+        //{
+        //    int remainder = 0, reverse = 0;
+        //    while (number != 0)
+        //    {
+        //        remainder = number % 10;
+        //        reverse = (reverse * 10) + remainder;
+        //        number = number / 10;
+        //        Console.WriteLine("Reaminder:{0} number:{1} reverse:{2}", remainder, number, reverse);
+        //    }
+        //    Console.WriteLine("Reverse number is:" + reverse);
+        //}
+
+        public static void ReverseWord(string word)
+        {
+            string reverse = " ";
+            char[] listOfChar = word.ToCharArray();
+            for (int i = 0; i < listOfChar.Length; i++)
+            {
+                reverse = listOfChar[i] + reverse;
+            }
+            Console.WriteLine("Reverse = "+reverse);    
+        }
+    }
+}
